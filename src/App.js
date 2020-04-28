21cc688d16d1ec72b903bfc3d58a3efb6eacfdb1
jing yi (jane) yuan <jingyiyuan95@gmail.com>
2020-04-28 12:00:26 -0400

    added header

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './styles/styles.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      
+      <header>
+        <h1>COOKS "R" US</h1>
+        <p>How to Cook</p>
+        <form action="">
+          <input type="text" placeholder="enter an ingredient"/>
+        </form>
       </header>
+      {/* <a href="https://www.freepik.com/free-photos-vectors/food">Food vector created by photographeeasia - www.freepik.com</a> */}
     </div>
   );
 }
